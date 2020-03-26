d76e912d81d30d0df7e9e59dc4b9e9cdb961a812
Paras <spunater@yahoo.com>
2020-03-26 01:05:22 -0400

    Now able to publish integer to channel field.

--- mqtt.py
+++ mqtt.py
@@ -6,14 +6,14 @@ class ServoStatusRequest:
         self.mqtt_url = 'mqtt.thingspeak.com'
         self.write_api = '6AXCGP0YJN2QUFEQ'
         self.read_api = 'UTOB3C6YT3AZMEZN'
-        self.current_status_topic = 'channels/1001649/publish/fields/field1/{}'.format(self.write_api)
+        self.current_status_topic = 'channels/1001649/publish/{}'.format(self.write_api)
         self.requested_status_topic = 'channels/1001649/subscribe/fields/field2/{}'.format(self.read_api)
 
     def modify_servo_pos(self, topic, message):
         print(message)
     #     Add code to modify servo pos. according to request and then update servo status
 
-    def mqtt_subscribe_to_request(self):
+    def MQTT_subscribe_to_request(self):
         self.check_messages = True
         client = MQTTClient(self.mqtt_url)
         client.set_callback(self.modify_servo_pos)
@@ -23,8 +23,8 @@ class ServoStatusRequest:
             time.sleep(5)
         client.disconnect()
 
-    def MQTT_publish(self, value:str):
+    def MQTT_publish(self, value):
         client = MQTTClient('esp_mqtt', self.mqtt_url)
         client.connect()
-        client.publish(self.current_status_topic.encode(), value.encode())
+        client.publish(self.current_status_topic.encode(), 'field1={}'.format(str(value)).encode())
         client.disconnect()
